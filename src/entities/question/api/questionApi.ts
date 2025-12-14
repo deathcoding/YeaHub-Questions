e77a1804dd36b93b72ff2c8cbd5b36db8b086118
deathCodinn <ilgiz.ilishev@gmail.com>
2025-12-14 22:49:13 +0500

    refactor getQuestionsList endpoint to remove mock handling and streamline query

--- src/entities/question/api/questionApi.ts
+++ src/entities/question/api/questionApi.ts
@@ -4,20 +4,11 @@ import type {
   GetQuestionByIdResponse,
   GetQuestionsList,
 } from "../model/question.types";
-import { getQuestionListMock } from "@/shared/api/mocks/getQuestionListMock";
-
-const USE_MOCKS = import.meta.env.VITE_USE_MOCKS === "true";
 
 const questionApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getQuestionsList: builder.query<GetQuestionsList, void>({
-      async queryFn(_arg, _api, _extraOptions, baseQuery) {
-        if (USE_MOCKS) {
-          return { data: await getQuestionListMock() };
-        }
-        const result = await baseQuery("/questions/public-questions");
-        return { data: result.data as GetQuestionsList };
-      },
+      query: () => "/questions/public-questions",
     }),
     getQuestionById: builder.query<
       GetQuestionByIdResponse,
